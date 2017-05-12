fab3465fb4e79b8712041ce5d01ee240655d8d95
ErisvaldoCarvalho <erisvaldocarvalhosilva@gmail.com>
2017-05-12 16:47:06 -0300

    Realizada manutenção na procedure de importação

--- Manutencao/Script auxiliar.sql
+++ Manutencao/Script auxiliar.sql
@@ -1,73 +1,17 @@
 
-
-PRINT 'Criar objetos para manutenção'
-
-GO
-
-GO
-PRINT 'VERIFICAR A DUPLICIDADE DA PROC SP_ImportarTabela'
-drop proc SP_ImportarTabela
+IF(EXISTS(SELECT 1 FROM SYS.objects WHERE NAME = 'SP_OMGERARSCRIPTBASICO'))
+	DROP PROC SP_OMGERARSCRIPTBASICO
+IF(EXISTS(SELECT 1 FROM SYS.objects WHERE NAME = 'SP_OMReindexarTabelas'))
+	DROP PROC SP_OMReindexarTabelas
+IF(EXISTS(SELECT 1 FROM SYS.objects WHERE NAME = 'VW_OMColunas'))
+	DROP VIEW VW_OMColunas
+IF(EXISTS(SELECT 1 FROM SYS.objects WHERE NAME = 'VW_OMRelacionamento'))
+	DROP VIEW VW_OMRelacionamento
+IF(EXISTS(SELECT 1 FROM SYS.objects WHERE NAME = 'Vw_ChecarRelacionamento'))
+	DROP VIEW Vw_ChecarRelacionamento
+IF(EXISTS(SELECT 1 FROM SYS.objects WHERE NAME = 'SP_ImportarTabela'))
+	DROP PROC SP_ImportarTabela
 go
-CREATE PROC SP_ImportarTabela
-      @BancoOrigem VARCHAR(100),
-      @BancoDestino VARCHAR(100),
-      @TabelaOrigem VARCHAR(100),
-      @TabelaDestino VARCHAR(100)
-AS
-DECLARE @Coluna varchar(100)
-DECLARE @ColunasDestino VARCHAR(MAX) = ''
-DECLARE @ColunasOrigem VARCHAR(MAX) = ''
-DECLARE @Amarracao VARCHAR(MAX) = ''
-DECLARE @ValidarNulidade VARCHAR(100) = '1 = 1'
-DECLARE @SQL VARCHAR(MAX)
-
-SELECT COLUNA, CONSTRAINT_NAME  INTO #TEMP FROM VW_OMColunas WHERE TABELA = @TABELAORIGEM
-
-
-WHILE(EXISTS(SELECT 1 FROM #TEMP))
-BEGIN
-      SET @Coluna = (SELECT TOP 1 COLUNA FROM #TEMP)
-      SET @ColunasDestino = @ColunasDestino + @Coluna + ','
-      SET @ColunasOrigem = @ColunasOrigem + ' T.' + @Coluna + ','
-     
-      IF(EXISTS(SELECT 1 FROM #TEMP WHERE CONSTRAINT_NAME IS NOT NULL AND COLUNA = @Coluna))
-      BEGIN
-            IF(LEN(@Amarracao)>1)
-            SET @Amarracao = @Amarracao + ' AND'
-            SET @Amarracao = @Amarracao + 'T.' + @Coluna + ' = T1.' + @Coluna
-            SET @ValidarNulidade = @Coluna + ' IS NULL'
-      END
-     
-      DELETE FROM #TEMP where COLUNA = @Coluna
-END
-
-SET @ColunasDestino = left(@ColunasDestino,LEN(@ColunasDestino)-1)
-SET @ColunasOrigem = left(@ColunasOrigem,LEN(@ColunasOrigem)-1)
-
-SELECT @Amarracao
-IF(LEN(@Amarracao)<1)
-BEGIN
-SET @SQL = '
-INSERT INTO ' + @BancoDestino + '..' + @TabelaOrigem + '(' + @ColunasDestino + ')
-SELECT ' + @ColunasOrigem + ' FROM ' + @BancoOrigem + '..' + @TabelaOrigem + ' T'
-
-END
-ELSE
-BEGIN
-SET @SQL = '
-INSERT INTO ' + @BancoDestino + '..' + @TabelaOrigem + '(' + @ColunasDestino + ')
-SELECT ' + @ColunasOrigem + ' FROM ' + @BancoOrigem + '..' + @TabelaOrigem + ' T
-LEFT JOIN ' + @BancoDestino + '..' + @TabelaDestino + ' T1 ON ' + @Amarracao + '
-WHERE T1.' + @ValidarNulidade
-END
-
-PRINT @SQL
-
-DROP TABLE #TEMP
-
-
-GO
-
 
 CREATE PROC SP_OMGerarScriptBasico
       @TABELA VARCHAR(500)
@@ -171,22 +115,12 @@ GO
 CREATE VIEW Vw_ChecarRelacionamento
 
 AS
---esta view foi criada para listar as colunas que possivelmente devam ser relacionadas, mas fogem da padronização de nomes
+--esta view foi criada para listar as colunas que possivelmente devam ser relacionadas, mas fogem da padronizaÃ§Ã£o de nomes
 SELECT syscolumns.name Coluna,sysobjects.name Tabela FROM SYScolumns INNER JOIN SYSOBJECTS ON syscolumns.id = sysobjects.id WHERE SYSCOLUMNS.name LIKE 'Codigo%' and SYSCOLUMNS.name <>'cODIGO'
 AND SUBSTRING(SYSCOLUMNS.name,7,20) NOT IN(SELECT SYSOBJECTS.name FROM SYSOBJECTS WHERE XTYPE = 'U')
 AND sysobjects.xtype = 'U'
 
 GO
-PRINT 'Remover objetos criados para manutenção'
-GO
-DROP PROC SP_OMGERARSCRIPTBASICO
-DROP PROC SP_OMReindexarTabelas
-DROP VIEW VW_OMColunas
-DROP VIEW VW_OMRelacionamento
-DROP VIEW Vw_ChecarRelacionamento
-DROP PROC SP_ImportarTabela
-GO
-
 
 CREATE PROC SP_ImportarTabela
       @BancoOrigem VARCHAR(100),
@@ -195,25 +129,37 @@ CREATE PROC SP_ImportarTabela
       @TabelaDestino VARCHAR(100)
 AS
 DECLARE @Coluna varchar(100)
-DECLARE @ColunasDestino VARCHAR(MAX) = ''
-DECLARE @ColunasOrigem VARCHAR(MAX) = ''
+DECLARE @ColunasDestino1 VARCHAR(MAX) = ''
+DECLARE @ColunasOrigem1 VARCHAR(MAX) = ''
+DECLARE @ColunasDestino2 VARCHAR(MAX) = ''
+DECLARE @ColunasOrigem2 VARCHAR(MAX) = ''
 DECLARE @Amarracao VARCHAR(MAX) = ''
 DECLARE @ValidarNulidade VARCHAR(100) = '1 = 1'
 DECLARE @SQL VARCHAR(MAX)
+DECLARE @Colorder INT = 0
 
-SELECT COLUNA, CONSTRAINT_NAME  INTO #TEMP FROM VW_OMColunas WHERE TABELA = @TABELAORIGEM
+SELECT COLUNA, CONSTRAINT_NAME,colorder  INTO #TEMP FROM VW_OMColunas WHERE TABELA = @TABELAORIGEM
 
 
 WHILE(EXISTS(SELECT 1 FROM #TEMP))
 BEGIN
-      SET @Coluna = (SELECT TOP 1 COLUNA FROM #TEMP)
-      SET @ColunasDestino = @ColunasDestino + @Coluna + ','
-      SET @ColunasOrigem = @ColunasOrigem + ' T.' + @Coluna + ','
+	  SET @Colorder = (SELECT MIN(colorder) FROM #TEMP)
+      SET @Coluna = (SELECT TOP 1 COLUNA FROM #TEMP WHERE colorder = @Colorder)
+      
+	  IF(LEN(@ColunasDestino1)<6000)
+		SET @ColunasDestino1 = @ColunasDestino1 + @Coluna + ','
+	  ELSE
+		SET @ColunasDestino2 = @ColunasDestino2 + @Coluna + ','
+      IF(LEN(@ColunasOrigem1)<6000)
+		SET @ColunasOrigem1 = @ColunasOrigem1 + ' T.' + @Coluna + ','
+	  ELSE
+		SET @ColunasOrigem2 = @ColunasOrigem2 + ' T.' + @Coluna + ','
+
      
       IF(EXISTS(SELECT 1 FROM #TEMP WHERE CONSTRAINT_NAME IS NOT NULL AND COLUNA = @Coluna))
       BEGIN
             IF(LEN(@Amarracao)>1)
-            SET @Amarracao = @Amarracao + ' AND'
+            SET @Amarracao = @Amarracao + ' AND '
             SET @Amarracao = @Amarracao + 'T.' + @Coluna + ' = T1.' + @Coluna
             SET @ValidarNulidade = @Coluna + ' IS NULL'
       END
@@ -221,24 +167,45 @@ BEGIN
       DELETE FROM #TEMP where COLUNA = @Coluna
 END
 
-SET @ColunasDestino = left(@ColunasDestino,LEN(@ColunasDestino)-1)
-SET @ColunasOrigem = left(@ColunasOrigem,LEN(@ColunasOrigem)-1)
+IF(LEN(@ColunasDestino2)>0)
+	SET @ColunasDestino2 = left(@ColunasDestino2,LEN(@ColunasDestino2)-1) + ')'
+ELSE
+	SET @ColunasDestino1 = left(@ColunasDestino1,LEN(@ColunasDestino1)-1) + ')'
+IF(LEN(@ColunasOrigem2)>0)
+	SET @ColunasOrigem2 = left(@ColunasOrigem2,LEN(@ColunasOrigem2)-1)
+ELSE
+	SET @ColunasOrigem1 = left(@ColunasOrigem1,LEN(@ColunasOrigem1)-1)
 
-SELECT @Amarracao
 IF(LEN(@Amarracao)<1)
 BEGIN
-SET @SQL = '
-INSERT INTO ' + @BancoDestino + '..' + @TabelaOrigem + '(' + @ColunasDestino + ')
-SELECT ' + @ColunasOrigem + ' FROM ' + @BancoOrigem + '..' + @TabelaOrigem + ' T'
+
+PRINT 'PRINT ''Tabela: '+ @TabelaOrigem +'''
+IF(NOT EXISTS(SELECT 1 FROM ' + @BancoDestino + '..' + @TabelaOrigem + '))
+INSERT INTO ' + @BancoDestino + '..' + @TabelaOrigem + '(' 
+PRINT @ColunasDestino1 
+IF(LEN(@ColunasDestino2)>0)
+PRINT @ColunasDestino2 
+PRINT'SELECT ' + @ColunasOrigem1 
+IF(LEN(@ColunasOrigem2)>0)
+PRINT @ColunasOrigem2 
+PRINT 'FROM ' + @BancoOrigem + '..' + @TabelaOrigem + ' T
+GO'
 
 END
 ELSE
 BEGIN
-SET @SQL = '
-INSERT INTO ' + @BancoDestino + '..' + @TabelaOrigem + '(' + @ColunasDestino + ')
-SELECT ' + @ColunasOrigem + ' FROM ' + @BancoOrigem + '..' + @TabelaOrigem + ' T
+PRINT 'PRINT ''Tabela: '+ @TabelaOrigem +'''
+INSERT INTO ' + @BancoDestino + '..' + @TabelaOrigem + '(' 
++ @ColunasDestino1 
+IF(LEN(@ColunasDestino2)>0)
+PRINT @ColunasDestino2 
+PRINT 'SELECT ' + @ColunasOrigem1 
+IF(LEN(@ColunasOrigem2)>0)
+PRINT @ColunasOrigem2 
+PRINT 'FROM ' + @BancoOrigem + '..' + @TabelaOrigem + ' T
 LEFT JOIN ' + @BancoDestino + '..' + @TabelaDestino + ' T1 ON ' + @Amarracao + '
-WHERE T1.' + @ValidarNulidade
+WHERE T1.' + @ValidarNulidade + '
+GO'
 END
 
 PRINT @SQL
